444ea7ea2012f163280fa6230fd364169f6f6cde
Md Ataullah Masood <info.mdataullah@gmail.com>
2026-06-03 12:59:49 +0530

    (add): QnA ChatBot with real time google search

---
+++ apps/internetSearchAgent.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain.agents import create_agent
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+llm = ChatGroq(model="openai/gpt-oss-20b")
+
+tavily_search_tool = TavilySearch(
+    max_results = 3,
+    topic = "general"
+)
+
+agent = create_agent(
+    model= llm,
+    tools=[tavily_search_tool],
+    system_prompt="You are a agent and can search any query on internet"
+)
+
+while True:
+    user_query = input("USER : ")
+    if user_query.lower() in ["quit","stop","exit"]:
+        print("Stopped.")
+        break
+    
+    res = agent.invoke({"messages":[{"role":"user" , "content":user_query}]})
+    
+    r = res["messages"][-1].content
+    
+    print("AI :", r)   #? this will answer directly => no streaming like answer
+    
+    
